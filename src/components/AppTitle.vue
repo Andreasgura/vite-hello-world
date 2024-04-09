dfa933c750ee92810ab77deb7ba9c9b0c8a45d7e
Andreasgura <andre-56@hotmail.it>
2024-04-09 17:48:24 +0200

    AppTitle component

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <header>
+            <h1>{{ myTitle }}</h1>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+        data(){
+            return {
+                myTitle : 'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
